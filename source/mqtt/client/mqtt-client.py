de2984b7aa3d7da0a4fdbc261a435002bec741d3
Kalana Sachith <kal25mahaarachchi@gmail.com>
2023-12-23 19:47:16 +0100

    initial client code

---
+++ source/mqtt/client/mqtt-client.py
@@ -0,0 +1,63 @@
+import random
+from paho.mqtt import client as mqtt_client
+import paho.mqtt.client as mqtt
+
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
+
+broker = "192.168.1.78"	
+pub_topic = "iotproject/group788/prox"
+port = 1883
+client_id = f'mqtt-benchmark-client{random.randint(0, 1000)}'
+
+def connect_mqtt():
+    
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+            
+    def on_disconnect(client, userdata, rc):
+        logging.info("Disconnected with result code: %s", rc)
+        reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+        while reconnect_count < MAX_RECONNECT_COUNT:
+            logging.info("Reconnecting in %d seconds...", reconnect_delay)
+            time.sleep(reconnect_delay)
+
+            try:
+                client.reconnect()
+                logging.info("Reconnected successfully!")
+                return
+            except Exception as err:
+                logging.error("%s. Reconnect failed. Retrying...", err)
+
+            reconnect_delay *= RECONNECT_RATE
+            reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+            reconnect_count += 1
+        logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+        
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+run()
