e6e5bfe98bac27fcc075f933871d81bb3ebd3787
Kalana Sachith <kal25mahaarachchi@gmail.com>
2023-12-27 21:46:45 +0100

    Add subcriber client in oop way

--- source/mqtt/server/mqtt-speed-benchmark-server.py
+++ source/mqtt/server/mqtt-speed-benchmark-server.py
@@ -1,16 +1,106 @@
 import paho.mqtt.client as mqtt
-import time, random, sys , os
+from paho.mqtt import client as mqtt_client
+import time, random, sys , os,logging
 import json
 
 sys.path.append(os.path.abspath(os.path.join('source','sensor')))
 from proximity_decoy_sensor import get_proximity
 
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
 
 broker = "192.168.1.83"	# Broker 
-pub_topic = "iotproject/group788/prox"       # send messages to this topic
+pub_sub_topic = "iotproject/group788/prox"       # send messages to this topic
 port = 1883
 no_of_test_runs = 200
 
+class Subscriber:
+    def __init__(self, client_id, broker,port,sub_topic) -> None:
+        self.time_list = []
+        self.client_id = client_id
+        self.broker = broker
+        self.port = port
+        self.topic = sub_topic
+    
+    def __get_client_id(self):
+        return f'mqtt-speed-bench-sub {self.client_id}'
+    
+    def __get_client(self):
+        client_id = self.__get_client_id()
+        user_data = client_id
+        self.client = mqtt.Client(client_id,userdata= user_data)
+        return self.client
+    
+    def __config_client(self)-> None:
+        self.client = self.__get_client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
+
+    def set_no_of_test_cases(self, no):
+        self.no_of_messages = no
+        
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    def on_disconnect(self, client, userdata, rc):
+        logging.info("Disconnected with result code: %s", rc)
+        reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+        while reconnect_count < MAX_RECONNECT_COUNT:
+            logging.info("Reconnecting in %d seconds...", reconnect_delay)
+            time.sleep(reconnect_delay)
+
+            try:
+                client.reconnect()
+                logging.info("Reconnected successfully!")
+                return
+            except Exception as err:
+                logging.error("%s. Reconnect failed. Retrying...", err)
+
+            reconnect_delay *= RECONNECT_RATE
+            reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+            reconnect_count += 1
+        logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+        
+    def on_message(self, client, userdata, msg):
+        data = json.loads(msg.payload.decode())
+        finish_time = time.time()
+        self.time_list.append((finish_time,data['start_time']))
+        # print("Received ",msg.payload.decode()," from ",msg.topic," Topic")
+        
+    def run_client(self):
+        try:
+            self.__config_client()
+            time_with_connection = time.time()
+            self.client.connect(self.broker, self.port)
+            self.client.subscribe(self.topic)
+            self.client.loop_start()
+            while len(self.time_list) < no_of_test_runs:
+                pass
+            self.calculate_execution_speed()
+            print("Stopping client")
+            self.client.loop_stop()
+        except KeyboardInterrupt:
+            self.client.disconnect()
+            self.client.loop_stop()  
+
+    def calculate_execution_speed(self):
+        difference =[]
+        for time_tuple in self.time_list:
+            end_time,start_time = time_tuple
+            difference.append(end_time-start_time)
+        average_speed = sum(difference)/len(difference)
+        print("Average speed", average_speed, sep="\t")
+
+
+sub = Subscriber(random.randint(0,1000),broker,port,pub_sub_topic)
+sub.set_no_of_test_cases(no_of_test_runs)
+sub.run_client()
 
 class Publisher:
     
@@ -93,6 +183,6 @@ class Publisher:
         self.port = port
         self.topic = topic
         
-pub = Publisher(random.randint(0, 1000),broker,port,pub_topic)
-pub.set_no_of_test_cases(no_of_test_runs)
-pub.run_client()
+# pub = Publisher(random.randint(0, 1000),broker,port,pub_sub_topic)
+# pub.set_no_of_test_cases(no_of_test_runs)
+# pub.run_client()
