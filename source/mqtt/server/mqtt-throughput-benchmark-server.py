8c4ba6a8093e060f7028ad5ec4feb35c12091e97
Kalana Sachith <kal25mahaarachchi@gmail.com>
2023-12-28 23:24:04 +0100

    publish benchmark result

--- source/mqtt/server/mqtt-throughput-benchmark-server.py
+++ source/mqtt/server/mqtt-throughput-benchmark-server.py
@@ -1,13 +1,14 @@
 import paho.mqtt.client as mqtt
-import time, random, sys , os, threading
+import time, random, sys , os, threading,json
 
 sys.path.append(os.path.abspath(os.path.join('source','sensor')))
 from proximity_sensor import get_proximity
 
 # Parameters
-broker = "192.168.1.78"
+broker = "192.168.1.83"
 port = 1883
 topic = "iotproject/group788/prox"
+topic_result = "iotproject/group788/benchmark/throughput"
 num_clients = 80
 num_test_cases = 150
 
@@ -27,6 +28,52 @@ class ThroughputCalculator:
     self.throughput = self.success_messages*100/self.total_messages
     return self.throughput
 
+class ResultPublisher:
+  def on_connect(self, client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+  def on_publish(self, client, userdata, mid):
+    print(f"------Benchmark result {mid} published------")
+  
+  def __get_client_id(self):
+    self.client_id = f'mqtt-benchmark-publisher {random.randint(0,1000)}'
+  
+  def __get_client(self):
+    client_id = self.__get_client_id()
+    user_data = client_id
+    self.client = mqtt.Client(client_id,userdata= user_data)
+    return self.client
+  
+  def set_message(self,data):
+     self.payload = data
+   
+  def run_client(self):
+    try:
+      self.__config_client()
+      self.client.connect(self.broker, self.port)
+      self.client.loop_start()
+      self.client.publish(self.topic, self.payload)
+      time.sleep(0.1)  # Adjust the delay between messages
+      self.client.disconnect()
+      self.client.loop_stop()
+    except KeyboardInterrupt:
+      self.client.disconnect()
+      self.client.loop_stop()
+  
+  def __config_client(self)-> None:
+    self.client = self.__get_client()
+    self.client.on_connect = self.on_connect
+    self.client.on_publish = self.on_publish
+   
+  def __init__(self, broker, port,topic):
+    self.broker = broker
+    self.port = port
+    self.topic = topic
+     
+  
 class Publisher:
   
   def on_connect(self, client, userdata, flags, rc):
@@ -108,7 +155,12 @@ try:
   for thread in threads:
       thread.join()
   print("All publishers finished")
-  calculate_summary()
+  avg_tp, loss = calculate_summary()
+  # Now result will be publish to same broker
+  summary_pub = ResultPublisher(broker,port,topic_result)
+  summary_pub.set_message(json.dumps({'average_tp': avg_tp, 'loss': loss}))
+  summary_pub.run_client()
+  
 except KeyboardInterrupt:
     print("\nStopping clients...")
     for thread in threads:
