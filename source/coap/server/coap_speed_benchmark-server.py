954f298afcd47635a0b4921e1274d50e66ca7f2a
Kalana Sachith <kal25mahaarachchi@gmail.com>
2023-12-29 17:05:15 +0100

    coap server part is done

--- source/coap/server/coap_speed_benchmark-server.py
+++ source/coap/server/coap_speed_benchmark-server.py
@@ -1,11 +1,31 @@
-import asyncio,json
-from aiocoap import *
+import datetime
+import logging
+import asyncio
+import aiocoap
+import aiocoap.resource as resource
+from aiocoap.numbers.contentformat import ContentFormat
+from sensor_resource.proximity import ProximitySensorResource
 
-async def main():
-    protocol = await Context.create_client_context()
-    msg = Message(code=GET, uri="coap://192.168.1.78:3030/sensor/proximity")
-    for _ in range(100):
-        response = await protocol.request(msg).response
-        print(json.loads(response.payload))
 
-asyncio.run(main())
+class CoAPServer:
+    def __init__(self) -> None:
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger("coap-server").setLevel(logging.DEBUG)
+        
+    async def start_server(self):
+        print("--Server start --")
+        root = resource.Site()
+        root.add_resource(['.well-known', 'core'],
+                resource.WKCResource(root.get_resources_as_linkheader))
+        root.add_resource(['sensor','proximity'],ProximitySensorResource())
+
+        await aiocoap.Context.create_server_context(site=root,bind=("192.168.0.106",3030))
+        print("Server context created")
+
+        # Run forever
+        await asyncio.get_running_loop().create_future()
+        print("--Server end --")
+        
+if __name__ == "__main__":
+    server = CoAPServer()
+    asyncio.run(server.start_server())
